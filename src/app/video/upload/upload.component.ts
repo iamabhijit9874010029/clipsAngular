d98594845bb3817c264426c12b9a3f02a61b1381
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2025-02-03 01:40:43 +0530

    upload feature introduced

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-upload',
@@ -7,12 +9,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './upload.component.css'
 })
 export class UploadComponent {
+
+  constructor(private storage: AngularFireStorage) { }
+
   isDragOver = false;
   file: File | null = null;
   nextStep = false;
 
+  // uploadForm: FormGroup = new FormGroup({
+  //   title: new FormControl('', Validators.required)
+  // });
+
+  title: FormControl = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+    ],
+    nonNullable: true
+  })
+
   uploadForm: FormGroup = new FormGroup({
-    title: new FormControl('', Validators.required)
+    title: this.title
   });
 
   storeFile($event: Event) {
@@ -31,12 +48,50 @@ export class UploadComponent {
     console.log("succes - the MIME type or subtype of the file is : ", this.file.type);
 
     this.nextStep = true;
-    this.uploadForm.patchValue({ title: this.file.name });
+    // this.uploadForm.patchValue({ title: this.file.name });
+
+    // this.title.setValue(this.file.name.replace(".mp4", ""));
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ""));
   }
 
-  upload($event: Event) {
-    $event.preventDefault();
+  // upload($event: Event) {
+  //   $event.preventDefault();
+  //   console.log("uploading...");
+  //   console.log(this.uploadForm.value);
+  // }
+
+  uploadFIle() {
     console.log("uploading...");
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+    this.storage.upload(clipPath, this.file);
     console.log(this.uploadForm.value);
+    console.log("uploaded");
   }
+
+  // async uploadFIle() {
+  //   if (!this.file) {
+  //     console.error("No file selected for upload.");
+  //     return;
+  //   }
+
+  //   console.log("Uploading...");
+  //   const clipFileName = uuid();
+  //   const clipPath = `clips/${clipFileName}.mp4`;
+
+  //   const uploadTask = this.storage.upload(clipPath, this.file);
+
+  //   try {
+  //     await uploadTask.snapshotChanges().toPromise();
+  //     console.log("Upload successful", this.uploadForm.value);
+  //   } catch (error) {
+  //     console.error("Upload failed", error);
+  //   }
+  // }
+
+
 }
+
+
+// Your data location has been set in a region that does not support no-cost Storage buckets. Create or import a Cloud Storage bucket to get started.
+// https://www.youtube.com/watch?v=aE5gAHOhIok
