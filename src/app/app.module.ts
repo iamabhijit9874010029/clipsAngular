4a824790fc5da56d58c4a75cc3ae3074874538c4
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2025-04-20 02:08:42 +0530

    Lazy Loading Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { VideoModule } from './video/video.module';
+// import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
@@ -37,7 +37,7 @@ import { FbTimestampPipe } from "./pipes/fb-timestamp.pipe";
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    VideoModule,
+    // VideoModule,
     AppRoutingModule,
     AngularFireStorageModule,
     FbTimestampPipe
