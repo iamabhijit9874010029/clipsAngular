ff808588c1ee1db179d1887d59e2b79a3c2126dd
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2025-04-19 16:57:16 +0530

    Dynamically Rendering the Video

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import videojs from 'video.js';
+import IClip from '../models/clip.model';
+import { DatePipe } from '@angular/common';
 // import Player from 'video.js/dist/types/player';
 
 
@@ -8,10 +10,11 @@ import videojs from 'video.js';
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrl: './clip.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers: [DatePipe]
 })
 export class ClipComponent implements OnInit {
-  id = "";
+  clip?: IClip;
   @ViewChild("videoPlayer", { static: true }) target?: ElementRef;
   // player?: videojs.Player; //will not work in latest versions
   player?: ReturnType<typeof videojs>; // ✅ clean and future-proof
@@ -22,8 +25,14 @@ export class ClipComponent implements OnInit {
   ngOnInit(): void {
     this.player = videojs(this.target?.nativeElement);
     // this.id = route.snapshot.params['id'];
-    this.route.params.subscribe((params: Params) => {
-      this.id = params['id'];
+
+    this.route.data.subscribe(data=>{
+      this.clip = data['clip'] as IClip;
+
+      this.player?.src({
+        src: this.clip.url,
+        type: 'video/mp4'
+      })
     })
 
     // console.log(route.snapshot);
