25ab3de62d34912a7163653dffda11bcb83d48f1
Abhijit Haldar <abhijithaldar08@gmail.com>
2025-04-24 15:07:45 +0530

    Angular’s Test Bed Utility

---
+++ src/app/about/about.component.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from "@angular/core/testing"
+import { AboutComponent } from "./about.component"
+
+describe('About Component', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AboutComponent
+      ]
+    }).compileComponents()
+  })
+})
