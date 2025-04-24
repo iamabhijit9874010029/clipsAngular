6f048a674f2d706bfcb2436009dd6507163c39bc
Abhijit Haldar <abhijithaldar08@gmail.com>
2025-04-24 17:51:02 +0530

    Testing a Components Instance

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,12 +1,25 @@
-import { TestBed } from "@angular/core/testing"
+import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { AboutComponent } from "./about.component"
 
 describe('About Component', () => {
+  let fixture: ComponentFixture<AboutComponent>;
+  let component: AboutComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AboutComponent
       ]
     }).compileComponents()
-  })
-})
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create about component', () => {
+    expect(component).toBeTruthy();
+  });
+});
